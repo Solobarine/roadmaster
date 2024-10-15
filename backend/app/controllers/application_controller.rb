6856d7b3a7bc98f65809ca83eea6cc6ac3e70299
Solly <solobarine@gmail.com>
2024-10-15 16:26:04 +0100

    Refactor Code

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,25 +1,42 @@
 class ApplicationController < ActionController::API
   before_action :update_allowed_parameters, if: :devise_controller?
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :entity_not_authorized
+
+  def pundit_user
+    current_user || current_customer
+  end
 
   protected
 
   def update_allowed_parameters
-    if resource_class === Customer
-      devise_parameter_sanitizer.permit(:sign_up) do |u|
-        u.permit(:first_name, :last_name, :email, :password)
-      end
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:first_name, :last_surname, :email, :password, :current_password, :phone, :address, :city, :state,
-                 :zip_code)
-      end
-    elsif resource_class === User
-      devise_parameter_sanitizer.permit(:sign_up) do |u|
-        u.permit(:first_name, :last_name, :email, :password, :phone, :position, :hire_date)
-      end
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:first_name, :last_name, :email, :password, :phone, :position, :hire_date)
-      end
+    if resource_class == Customer
+      allow_customer_parameters
+    elsif resource_class == User
+      allow_user_parameters
     end
+  end
+
+  def allow_customer_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_surname, :email, :password, :current_password, :phone, :address, :city, :state,
+               :zip_code)
+    end
+  end
+
+  def allow_user_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :phone, :position, :hire_date)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :phone, :position, :hire_date)
+    end
+  end
+
+  def entity_not_authorized
+    render json: { error: 'Unauthorized' }, status: :forbidden
   end
 end
--- backend/app/controllers/users/current_user_controller.rb
+++ backend/app/controllers/users/current_user_controller.rb
@@ -1,7 +1,11 @@
 class Users::CurrentUserController < ApplicationController
   def index
-    render json: {
-      user: current_user.as_json(except: :jti)
-    }, status: :ok
+    if current_user
+      render json: {
+        user: current_user.as_json(except: :jti)
+      }, status: :ok
+    else
+      render json: { error: 'Unauthenticated' }, status: :unauthorized
+    end
   end
 end
