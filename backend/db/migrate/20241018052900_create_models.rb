aae96269d801646b3cd2a67377aa1ef34f04a1b8
Solly <solobarine@gmail.com>
2024-10-18 14:31:15 +0100

    Generate migration for Model Class

---
+++ backend/db/migrate/20241018052900_create_models.rb
@@ -0,0 +1,24 @@
+class CreateModels < ActiveRecord::Migration[7.1]
+  def change
+    create_table :models do |t|
+      t.references :brand, null: false, foreign_key: true
+      t.string :name
+      t.integer :year
+      t.string :body_type
+      t.string :engine_type
+      t.string :fuel_type
+      t.string :transmission
+      t.string :drivetrain
+      t.integer :horsepower
+      t.integer :torque
+      t.integer :seating_capacity
+      t.decimal :cargo_capacity, precision: 10, scale: 2
+      t.decimal :fuel_economy_city, precision: 5, scale: 2
+      t.decimal :fuel_economy_highway, precision: 5, scale: 2
+      t.decimal :safety_rating, precision: 3, scale: 1
+      t.decimal :msrp, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
