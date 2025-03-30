1e4997b1f28490477b6ebd28e7304a3c433dea49
Solly <solobarine@gmail.com>
2025-03-30 15:30:13 +0100

    make FAQ section

---
+++ client/components/home/FAQ.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="container px-4 flex-wrap sm:flex-nowrap mx-auto my-20 flex gap-10 items-center justify-between"
+  >
+    <div class="grow max-w-2xl">
+      <h3 class="text-3xl font-semibold">
+        Your Frequent Questions Answered. <br />
+        Check Here
+      </h3>
+      <div class="divide-y-2 divide-foreground">
+        <HomeFAQCard :key="index" v-for="(faq, index) in faqs" :faq="faq" />
+      </div>
+    </div>
+    <div class="grow shrink-0 grid place-items-center">
+      <img
+        src="/images/car_interior.jpg"
+        alt="Interior of a Car"
+        class="aspect-[4/3] w-80 objectobject-cover bg-foreground"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import faqs from "../../data/faq";
+</script>
---
+++ client/components/home/FAQCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="flex gap-10 items-center justify-between py-3">
+      <p class="font-semibold">{{ faq.question }}</p>
+      <button
+        type="button"
+        @click="toggleIsOpen"
+        class="text-2xl font-semibold"
+      >
+        {{ isOpen ? "-" : "+" }}
+      </button>
+    </div>
+    <p v-if="isOpen" class="text-sm text-text-secondary mb-6">
+      {{ faq.answer }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  faq: {
+    question: String,
+    answer: String,
+  },
+});
+
+const isOpen = ref(false);
+
+const toggleIsOpen = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
